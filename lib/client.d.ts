2e5134c83c9a6c49932320438af6d492022acd86
GitHub Action <action@github.com>
2020-10-08 17:08:52 +0000

    Build de nuevas dependencias

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -1,11 +1,57 @@
 import * as firebase from "firebase/app";
 /**
+ *  Representa los tipos de pago habilitados para la CLIENTA
+ */
+export declare enum PaymentMethod {
+    CARD = "Card",
+    CASH = "Cash",
+    SINPE = "Sinpe"
+}
+/**
+ * Representa el estado de la clienta en la aplicacion
+ * Util para los borrados lógicos de los clientes en el sistema
+ */
+export declare enum UserStatus {
+    ACTIVE = "Active",
+    INACTIVE = "Inactive"
+}
+/**
+ * Representa los lugares favoritos almacenados por el usuario
+ */
+export interface FavoritePlace {
+    /**
+     * @type {number}
+     * * index: index que el usuario le asigna a su lugar favorito almacenado.
+     */
+    index: number;
+    /**
+     * @type {string}
+     * * mapName: nombre definido por la API de mapas
+     */
+    mapName: string;
+    /**
+     * @type {string}
+     * * name: nombre al lugar asignado por la clienta
+     */
+    name: string;
+    /**
+     * @type {firebase.firestore.GeoPoint}
+     * * Geolocalización del lugar favorito
+     */
+    geoLocation: firebase.firestore.GeoPoint;
+    /**
+     * @type {string}
+     * * clientFavoritePlaceFirebaseID: id del lugar favorito
+     */
+    clientFavoritePlaceFirebaseID: string;
+}
+/**
  * * Representa un cliente del sistema
  */
 export interface Client {
     /**
      * @type {string}
-     * * clienFirebaseID: id unico del cliente en el sistema
+     * * clientFirebaseID: id unico del cliente en el sistema
      */
     clientFirebaseID: string;
     /**
@@ -15,17 +61,17 @@ export interface Client {
     firstName: string;
     /**
      * @type {string}
-     * * lastName: apellido del usario cliente
+     * * lastName: apellido del usuario cliente
      */
     lastName: string;
     /**
      * @type {string}
-     * * email: email del usario cliente
+     * * email: email del usuario cliente
      */
     email: string;
     /**
      * @type {string}
-     * * phone: telefono del usario cliente
+     * * phone: telefono del usuario cliente
      */
     phone: string;
     /**
@@ -38,34 +84,29 @@ export interface Client {
      * * profilePicURL: referencia de direccion de almacenamiento en Firebase de foto de perfil
      */
     profilePicURL: string;
+    /**
+     * @type {boolean}
+     * * passwordRequest: representa la condicion de solicitud de nueva contraseña por parte del usuario
+     */
+    passwordRequest: boolean;
+    /**
+     * @type {UserStatus}
+     * * userStatus: representa el estado del usuario en el sistema util para laos borrados lógicos
+     */
+    userStatus: UserStatus;
+    /**
+     * @type {FavoritePlace[]}
+     * * favoritePlaces: arreglo de instancias de lugares favoritos almacenados por la clienta
+     */
+    favoritePlaces: FavoritePlace[];
 }
 /**
- * * Representa ubicacion del sistema
- */
-export interface ClientLocation {
-    /**
-     * @type {string}
-     * * clienFirebaseID: referencia al id del cliente en el sistema
-     */
-    clientFirebaseID: string;
-    /**
-     * @type {firebase.firestore.Timestamp}
-     *  * timeStamp: estampa de tiempo en la que estuvo en la ubicacion
-     */
-    timeStamp: firebase.firestore.Timestamp;
-    /**
-     * @type {firebase.firestore.GeoPoint}
-     *  * currentLocation:  ubicacion del usario en el momento especifico
-     */
-    currentLocation: firebase.firestore.GeoPoint;
-}
-/**
- * * Representa los detalles para el rating del usario en un viaje especifico
+ * Representa el rating que la usuario hace sobre el viaje realizado
  */
 export interface ClientRideRating {
     /**
      * @type {string}
-     * * clienFirebaseID:  referencia al id del cliente en el sistema
+     * * clientFirebaseID:  referencia al id del cliente en el sistema
      */
     clientFirebaseID: string;
     /**
@@ -78,4 +119,99 @@ export interface ClientRideRating {
      * * clientRideRating:  referencia al id del viaje al cual la calificacion esta asociada
      */
     clientRideRating: number;
+}
+/**
+ * Solicitud hecha por la clienta
+ */
+export interface TripRequest {
+    /**
+     * @type {string}
+     * * clientFirebaseID:  id del cliente
+     */
+    clientFirebaseID: string;
+    /**
+     * @type {firebase.firestore.Timestamp}
+     * * requestTime:  momento en que la clienta hace la solicitud del viaje
+     */
+    requestTime: firebase.firestore.Timestamp;
+    /**
+     * @type {firebase.firestore.GeoPoint}
+     * * departureLocation: ubicación desde donde la clienta pide el viaje
+     */
+    departureLocation: firebase.firestore.GeoPoint;
+    /**
+     * @type {firebase.firestore.GeoPoint}
+     * * arrivalLocation: ubicación destino hacia donde la clienta se desplaza
+     */
+    arrivalLocation: firebase.firestore.GeoPoint;
+    /**
+     * @type {string}
+     * * paymentMethod:  tipo de pago que desea utilizar la clienta
+     */
+    paymentMethod: PaymentMethod;
+}
+/**
+ * Representa la solicitud de una nueva contraseña
+ */
+export interface ChangePasswordRequest {
+    /**
+     * @type {string}
+     * * clientFirebaseID:  id de la clienta
+     */
+    clientFirebaseID: string;
+    /**
+     * @type {string}
+     * * clientEmail:  email de la clienta
+     */
+    clientEmail: string;
+    /**
+     * @type {firebase.firestore.Timestamp}
+     * * requestTime:  estampilla de tiempo en que la clienta hace la solicitud de cambio de contraseña
+     */
+    requestTime: firebase.firestore.Timestamp;
+    /**
+     * @type {string}
+     * * code:  codigo usado por la clienta en la solicitud de nueva contraseña
+     */
+    code: string;
+}
+/**
+ * Representa la solicitud de hecha por la clienta para su verificación
+ */
+export interface ClientVerification {
+    /**
+     * @type {string}
+     * * clientFirebaseID:  id de la clienta
+     */
+    clientFirebaseID: string;
+    /**
+     * @type {string}
+     * * clientEmail:  email de la clienta
+     */
+    clientEmail: string;
+    /**
+     * @type {string}
+     * * verificationCode: codigo de verificacion dado a la clienta
+     */
+    verificationCode: string;
+}
+/**
+ * * Representa la ubicación de la clienta
+ */
+export interface ClientLocation {
+    /**
+     * @type {string}
+     * * clientFirebaseID: referencia al id del cliente en el sistema
+     */
+    clientFirebaseID: string;
+    /**
+     * @type {firebase.firestore.Timestamp}
+     *  * timeStamp: estampa de tiempo en la que estuvo en la ubicacion
+     */
+    timeStamp: firebase.firestore.Timestamp;
+    /**
+     * @type {firebase.firestore.GeoPoint}
+     *  * currentLocation:  ubicacion del usuario en el momento especifico
+     */
+    currentLocation: firebase.firestore.GeoPoint;
 }
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 import { Driver, Vehicle, DriverLocation, DriverRideRating, DriverDocuments, Marchamo, License, Insurance, Status, LicenseTypes } from "./driver";
 import { Ride, RideRequest, RideStatus, PaymentMethod } from "./rides";
-import { ClientLocation, ClientRideRating, Client } from "./client";
+import { ClientLocation, ClientRideRating, Client, FavoritePlace, TripRequest, ChangePasswordRequest, ClientVerification } from "./client";
 import { UserRoles } from "./userRoles";
-export { Driver, Vehicle, DriverLocation, DriverRideRating, DriverDocuments, Marchamo, License, Insurance, Ride, RideRequest, ClientLocation, ClientRideRating, Client, RideStatus, PaymentMethod, UserRoles, Status, LicenseTypes, };
+export { Driver, Vehicle, DriverLocation, DriverRideRating, DriverDocuments, Marchamo, License, Insurance, Ride, RideRequest, ClientLocation, ClientRideRating, Client, RideStatus, PaymentMethod, UserRoles, Status, LicenseTypes, FavoritePlace, TripRequest, ChangePasswordRequest, ClientVerification };
